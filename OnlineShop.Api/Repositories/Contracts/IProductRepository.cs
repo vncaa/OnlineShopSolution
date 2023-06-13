ba19d2e4c63b290c10ef903973f4d7b705366f4e
Václav Machovský <crocoo.main@gmail.com>
2023-06-13 15:15:41 +0200

    product repository

--- OnlineShop.Api/Repositories/Contracts/IProductRepository.cs
+++ OnlineShop.Api/Repositories/Contracts/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace OnlineShop.Api.Repositories.Contracts
+﻿using OnlineShop.Api.Entities;
+using System.Collections;
+
+namespace OnlineShop.Api.Repositories.Contracts
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<Product> GetItem(int id);
+        Task<ProductCategory> GetCategory(int id);
     }
 }
